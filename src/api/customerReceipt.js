59457f2ab2e4adb704487e40abc2edf8d0253e0e
yewon6282 <yewon6282@gmail.com>
2022-06-24 14:31:18 +0900

    status값 불러옴

--- src/api/customerReceipt.js
+++ src/api/customerReceipt.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//주문을 한 거래처의 이름과 주문 상태 가져오기
 async function getClientInfo() {
   let receiptList = [];
   try {
@@ -13,10 +14,11 @@ async function getClientInfo() {
   return receiptList;
 }
 
+//주문 상태 별 건수 가져오기
 async function getStatusCnt() {
   let status = [];
   try {
-    const response = await axios.get(`/client/`);
+    const response = await axios.get(`/client/sts`);
     status = response.data.statusCnt;
     console.log('status : ' + status);
   } catch (error) {
