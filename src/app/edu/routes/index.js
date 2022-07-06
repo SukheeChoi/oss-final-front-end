9e3756cf5c556b05a7b4c79c7e42260bb10ac6b4
Shin-HyunJu <afww@daum.net>
2022-07-06 11:33:50 +0900

    .

--- src/app/edu/routes/index.js
+++ src/app/edu/routes/index.js
@@ -9,70 +9,6 @@ export default [
       left: {
         show: false,
       },
-      children: [
-        // Test (2depth)
-        {
-          path: 'Main',
-          name: 'DISTSub',
-          redirect: '/edu/Order',
-        },
-        // detail ( 3depth)
-        {
-          path: 'CustomerReceipt',
-          name: 'DISTSub1',
-          component: () => import('@@/edu/views/CustomerReceipt'),
-        },
-        {
-          path: 'Order',
-          name: 'DISTSub2',
-          component: () => import('@@/edu/views/Order'),
-        },
-        {
-          path: 'ReleaseInspection',
-          name: 'DISTSub3',
-          component: () => import('@@/edu/views/ReleaseInspection'),
-        },
-        {
-          path: 'Releases',
-          name: 'DISTSub4',
-          component: () => import('@@/edu/views/Releases'),
-        },
-        {
-          path: 'Picking',
-          name: 'DISTSub5',
-          component: () => import('@@/edu/views/Picking'),
-        },
-        {
-          path: 'InspectionLabeling',
-          name: 'DISTSub6',
-          component: () => import('@@/edu/views/InspectionLabeling'),
-        },
-        {
-          path: 'test',
-          name: 'test',
-          component: () => import('@@/edu/views/Test'),
-        },
-        {
-          path: 'test1',
-          name: 'test1',
-          component: () => import('@@/edu/views/Test1'),
-        },
-        {
-          path: 'test2',
-          name: 'test2',
-          component: () => import('@@/edu/views/Test2'),
-        },
-        {
-          path: 'test3',
-          name: 'test3',
-          component: () => import('@@/edu/views/Test3'),
-        },
-        {
-          path: 'Copy',
-          name: 'DISTSub7',
-          component: () => import('@@/edu/views/Copy'),
-        },
-      ],
     },
     children: [
       // Test (2depth)
@@ -141,6 +77,11 @@ export default [
         path: 'test3',
         name: 'test3',
         component: () => import('@@/edu/views/Test3'),
+      },
+      {
+        path: 'Copy',
+        name: 'DISTSub7',
+        component: () => import('@@/edu/views/Copy'),
       },
     ],
   },
